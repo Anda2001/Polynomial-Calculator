976ffe14d3669cfc003a75b3382dc06a09bbed4a
Anda2001 <andadomsa17@gmail.com>
2022-02-25 15:30:49 +0200

    next

--- src/main/java/ro/tuc/pt/gui/View.java
+++ src/main/java/ro/tuc/pt/gui/View.java
@@ -1,4 +1,7 @@
 package ro.tuc.pt.gui;
+import javax.swing.*;
+import java.awt.*;
 
 public class View {
+
 }
--- src/main/java/ro/tuc/pt/logic/Operations.java
+++ src/main/java/ro/tuc/pt/logic/Operations.java
@@ -1,4 +1,16 @@
 package ro.tuc.pt.logic;
 
 public class Operations {
+
+    public int add(int firstPolynomial, int secondPolynomial) {
+        return firstPolynomial + secondPolynomial;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
 }
